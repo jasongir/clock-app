edac89fb8c5c8561a8a7f577a0e8dab40373fc9e
Jason <snujugroix@gmail.com>
2021-09-15 15:17:52 -0400

    using intervals now!

--- App.js
+++ App.js
@@ -3,19 +3,34 @@ import { Text, View, StyleSheet } from "react-native";
 import Constants from "expo-constants";
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: new Date(),
+		};
+	}
+
+	tick() {
+		this.setState({
+			date: new Date(),
+		});
+	}
+
 	componentDidMount() {
 		console.log("component did mount");
-		setInterval(() => {
-			this.forceUpdate();
+
+		this.timer = setInterval(() => {
+			this.tick();
 		}, 1000);
 	}
 	componentWillUnmount() {
 		console.log("component will unmount");
+		clearInterval(this.timer);
 	}
 	render() {
 		let clockElement = (
 			<View style={styles.container}>
-				<Text>The Time is Now :{new Date().toLocaleTimeString()}</Text>
+				<Text>The Time is Now: {this.state.date.toLocaleDateString()}</Text>
 			</View>
 		);
 		return clockElement;
